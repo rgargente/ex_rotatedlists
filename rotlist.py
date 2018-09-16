9e52d83f4c78fd34b239e3d193fdbb453fd1054e
rgargente <rgargente@gmail.com>
2018-09-16 16:39:40 +0100

    refactor

--- rotlist.py
+++ rotlist.py
@@ -20,6 +20,14 @@ def find_sortedunique(l, n):
 
 def find_sorted_nonunique_rotated(l, n):
     def find_first_different(l, repeated_i):
+        """
+        At this moment I think it's better to go the easiest way and just do a sequential search.
+        The binary searh is faster because we know something about the data, therefore we can discard big chunks of data.
+        In this case however we just don't know anything. We could try to do some kind of binary search, going left
+        first and then right, then looking in the gaps. However, in practice it'd be as fast as searching in random places
+        until the number is found. I don't think there is any reason to think that random is faster or better than
+        sequential. On top of it, sequential is definitely simpler!
+        """
         diff_i = None
         repeated_v = l[repeated_i]
         for i, v in enumerate(l[1:]):  # Skip the first element
@@ -40,7 +48,8 @@ def find_sorted_nonunique_rotated(l, n):
 
         if l[0] < value:  # still sorted, smaller values can be to the right, never to the left
             if n < l[min] or value < n:
-                # if our number is smaller than the beginning or bigger that the current one, we can discard the left size now
+                # if our number is smaller than the beginning or bigger that the current one,
+                # we can discard the left size now
                 min = i + 1
             else:  # let's just search to the left
                 i = get_middle(min, i)
@@ -56,8 +65,9 @@ def find_sorted_nonunique_rotated(l, n):
                 else:  # we don't know which side it is, let's just search to the left
                     i = get_middle(min, i)
 
-        if prev_i == i:  # if so far we haven't modified i (i.e. we don't want to search somewhere specific) let's find the new middle
-            prev_i = i
+        if prev_i == i:
+            # if so far we haven't modified i (i.e. we don't want to search somewhere specific),
+            # let's find the new middle
             i = get_middle(min, max)
             if prev_i == i: # it's possible that the middle returns the same value we already had when max-min==1 ...
                 i += 1 # ... in that case, let's try the next value this time
