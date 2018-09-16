dcc22d2875a3922fef8d310726f02e4a022cfc1c
rgargente <rgargente@gmail.com>
2018-09-16 15:18:07 +0100

    rotated non unique

--- rotlist.py
+++ rotlist.py
@@ -1,7 +1,7 @@
-def find_sortedunique(l, n):
-    def get_middle(min, max):
-        return int(min + (max - min) / 2)
+def get_middle(min, max):
+    return int(min + (max - min) / 2)
 
+def find_sortedunique(l, n):
     min = 0
     max = len(l) - 1
     i = 0
@@ -17,9 +17,16 @@ def find_sortedunique(l, n):
     return i if l[i] == n else None
 
 
-def find_sorteduniquerotated(l, n):
-    def get_middle(min, max):
-        return int(min + (max - min) / 2)
+def find_sorted_nonunique_rotated(l, n):
+
+    def find_first_different(l, repeated_i):
+        diff_i = None
+        repeated_v = l[repeated_i]
+        for i, v in enumerate(l[1:]):  # Skip the first element
+            if v != repeated_v:
+                diff_i = i + 1  # +1 because we skipped the first element
+                break
+        return diff_i
 
     min = 0
     max = len(l) - 1
@@ -29,20 +36,30 @@ def find_sorteduniquerotated(l, n):
         value = l[i]
         if value == n:
             return i
+
         if l[0] < value:  # still sorted, smaller values can be to the right, never to the left
-            if n < l[min] or value < n:  # if our number is smaller than the beginning or bigger that the current one, we can discard the left size now
+            if n < l[min] or value < n:
+                # if our number is smaller than the beginning or bigger that the current one, we can discard the left size now
                 min = i + 1
-            else:
+            else:  # let's just search to the left
                 if min > 0:
-                    i = get_middle(min, i)  # let's just search to the left
+                    i = get_middle(min, i)
                 else:
-                    i = get_middle(1, i)  # let's just search to the left
+                    i = get_middle(1, i)
                 i_set = True
-        elif i > 0:  # pass the rotation point, bigger values can be to the left
-            if n < value:  # if our number is smaller than the current value, it must be to the left
-                max = i - 1
-            else:  # otherwise, if it wasn't the current one, let's just discard it
-                min = i + 1
+        elif i > 0:
+            if l[0] == value:  # still  a repeated value, we've learned nothing
+                i = find_first_different(l, i)
+                i_set = True
+            else:  # pass the rotation point, bigger values can be to the left
+                if n < value:  # if our number is smaller than the current value, it must be to the left...
+                    max = i - 1
+                elif l[
+                    0] > n:  # ...if it's bigger, it has to be to the right if the first element was already bigger than n
+                    min = i + 1
+                else:  # we don't know which side it is, let's just search to the left
+                    i = get_middle(min, i)
+                    i_set = True
 
         if not i_set:
             i = get_middle(min, max)
--- test/test_rotlist.py
+++ test/test_rotlist.py
@@ -1,6 +1,11 @@
 import rotlist
+import pytest
 
 l = [2, 4, 5, 6, 7, 10, 12, 13, 20]
+
+
+def rotate(l, rotations):
+    return l[rotations:] + l[:rotations]
 
 
 def test_find_sorted_unique():
@@ -13,29 +18,40 @@ def test_non_existing_values_sorted_unique():
     assert rotlist.find_sortedunique(l, 100) is None  # Find big non existing value
 
 
-def rotate(l, rotations):
-    return l[rotations:] + l[:rotations]
-
-
-def test_find_sorted_unique_rotated():
+def test_find_sorted_nonunique_rotated_with_unique_list():
     for n in l:
-        print(n)
         for r in range(len(l)):
-            print(r)
             rl = rotate(l, r)
-            i = rotlist.find_sorteduniquerotated(rl, n)
-            try:
-                assert n == rl[i]
-            except:
-                print(n)
+            i = rotlist.find_sorted_nonunique_rotated(rl, n)
+            assert n == rl[i]
+
 
 
 def test_non_existing_values_rotated():
-    assert rotlist.find_sortedunique(l, 1) is None  # Find small non existing value
-    assert rotlist.find_sortedunique(l, 100) is None  # Find big non existing value
+    rl = rotate(l, 4)
+    assert rotlist.find_sortedunique(rl, 1) is None  # Find small non existing value
+    assert rotlist.find_sortedunique(rl, 100) is None  # Find big non existing value
 
 
 def test_repeated():
-    l = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1, 2, 3, 8, 8]
-    i = rotlist.find_sorteduniquerotated(l, 1)
+    l = [7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1, 2, 3]
+    i = rotlist.find_sorted_nonunique_rotated(l, 1)
     assert 1 == l[i]
+
+
+def test_repeated_hardcase():
+    l = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1, 2, 3, 8, 8]
+    i = rotlist.find_sorted_nonunique_rotated(l, 1)
+    assert 1 == l[i]
+
+    l = [8, 8, 8, 1, 2, 3, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
+    i = rotlist.find_sorted_nonunique_rotated(l, 1)
+    assert 1 == l[i]
+
+
+def test_non_existing_values_hardcase():
+    l = [8, 8, 8, 2, 3, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
+    assert rotlist.find_sortedunique(l, 1) is None  # Find small non existing value
+    assert rotlist.find_sortedunique(l, 100) is None  # Find big non existing value
+    l = [8] * 10
+    assert rotlist.find_sortedunique(l, 1) is None  # Find a non existing value where all values are the same
